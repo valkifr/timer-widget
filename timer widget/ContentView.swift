4cc913f56c727875a2ddc9922b88b983a7bba6d9
valkifr <161493732+valkifr@users.noreply.github.com>
2024-06-04 20:15:45 +0200

    Initial Commit

---
+++ timer widget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  timer widget
+//
+//  Created by Olivér Ungváry on 04/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ timer widget/timer_widgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  timer_widgetApp.swift
+//  timer widget
+//
+//  Created by Olivér Ungváry on 04/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct timer_widgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
