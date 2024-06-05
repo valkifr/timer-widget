b5efcf0aedd71d9381a415ca0046c31df3e43e4d
valkifr <161493732+valkifr@users.noreply.github.com>
2024-06-05 19:57:45 +0200

    qawsederrrrtgtgzhzhzu

---
+++ timer widget/ButtonConfiguations.swift
@@ -0,0 +1,23 @@
+//
+//  ButtonConfiguatio.swift
+//  timer widget
+//
+//  Created by Olivér Ungváry on 05/06/2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct AddButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(Color(red: 0.118, green: 0.141, blue: 0.376))
+            .foregroundStyle(.white)
+            .clipShape(Capsule())
+    }
+}
+
+func AddNewCountdown() {
+print("kys")
+}
---
+++ timer widget/ListItems.swift
@@ -0,0 +1,9 @@
+//
+//  ListItems.swift
+//  timer widget
+//
+//  Created by Olivér Ungváry on 05/06/2024.
+//
+import Foundation
+
+
