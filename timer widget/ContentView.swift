c27a9243e0530c8d04775f5ec56bf066331afb08
valkifr <161493732+valkifr@users.noreply.github.com>
2024-06-26 16:41:26 +0200

    add fegdfbxdxghgchvhgxv

--- timer widget/ContentView.swift
+++ timer widget/ContentView.swift
@@ -16,18 +16,20 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             NavigationStack {
+               
                     List() {
                     Text("date placeholder")
                             .padding(EdgeInsets(top: 7, leading: 0, bottom: 8.7, trailing: 10))
                     }
                     .navigationTitle("Countdowns")
                 }
+            
                 VStack {
                 Spacer()
                 Button("Add new countdown", systemImage: "plus.circle", action: {
                     isShowing.toggle()
                 })
-                .buttonStyle(AddButton())
+                .buttonStyle(AddCountdownButton())
                 .sheet(isPresented: $isShowing, content: {
                     SheetContentView()
                     })
